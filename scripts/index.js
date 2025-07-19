efa98b31544d55734ea65557b64b588ed678dac2
Ryan Ellerbroek <ryanplays.re@gmail.com>
2025-07-19 10:18:15 -0400

    Refactor modal functionality: consolidate open/close functions and streamline event listeners

--- scripts/index.js
+++ scripts/index.js
@@ -26,6 +26,15 @@ const initialCards = [
   },
 ];
 
+// Reusable modal functions
+function openModal(modal) {
+  modal.classList.add("modal_is-opened");
+}
+
+function closeModal(modal) {
+  modal.classList.remove("modal_is-opened");
+}
+
 // Edit Profile Modal
 const editProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
@@ -42,31 +51,22 @@ const editProfileBioInput =
 const profileNameEl = document.querySelector(".profile__name");
 const profileBioEl = document.querySelector(".profile__bio");
 
-function openModal(modal) {
-  modal.classList.add("modal_is-opened");
-}
-
-function closeModal(modal) {
-  modal.classList.remove("modal_is-opened");
-}
-
 editProfileButton.addEventListener("click", function () {
-  openModal(editProfileModal);
   editProfileNameInput.value = profileNameEl.textContent;
   editProfileBioInput.value = profileBioEl.textContent;
+  openModal(editProfileModal);
 });
+
 editProfileCloseButton.addEventListener("click", function () {
   closeModal(editProfileModal);
 });
 
-function handleEditProfileSubmit(evt) {
+editProfileForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   profileNameEl.textContent = editProfileNameInput.value;
   profileBioEl.textContent = editProfileBioInput.value;
   closeModal(editProfileModal);
-}
-
-editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+});
 
 // New Post Modal
 const newPostButton = document.querySelector(".profile__add-button");
@@ -79,18 +79,17 @@ const postCaptionInput = newPostModal.querySelector("#post-caption-input");
 newPostButton.addEventListener("click", function () {
   openModal(newPostModal);
 });
+
 newPostCloseButton.addEventListener("click", function () {
   closeModal(newPostModal);
 });
 
-function handleNewPostSubmit(evt) {
+newPostForm.addEventListener("submit", function (evt) {
   evt.preventDefault();
   console.log("Image Link:", postLinkInput.value);
   console.log("Caption:", postCaptionInput.value);
   closeModal(newPostModal);
-}
-
-newPostForm.addEventListener("submit", handleNewPostSubmit);
+});
 
 // Initial Cards Loop
 initialCards.forEach((card) => {
